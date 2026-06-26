977c6a306f631e2aeac73d378a21b3ee87a06d4c
midirectservice-bit <midirectservice@gmail.com>
2026-06-26 00:46:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// Esta parte verifica el token de Meta
 app.get('/webhook', (req, res) => {
-  res.status(200).send(req.query['hub.challenge']);
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === '123tokenrender123') {
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
 });
 
 app.post('/webhook', (req, res) => {
